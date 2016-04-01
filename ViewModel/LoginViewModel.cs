93ed72d8a0e160bb4b5b2d92876daea36c2c9367
mtayal <mtayal@ishir.com>
2016-04-01 20:32:58 +0530

    css changes in login page

--- ViewModel/LoginViewModel.cs
+++ ViewModel/LoginViewModel.cs
@@ -15,6 +15,10 @@ namespace ViewModel
 
        [Required]
        [DataType(DataType.EmailAddress)]
+       [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+                           @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+                           @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$",
+                           ErrorMessage = "Email is not valid")] 
        [Display(Name = "Email")]
        public string Email { get; set; }
 
